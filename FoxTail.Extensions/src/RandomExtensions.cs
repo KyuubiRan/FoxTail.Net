4497e6907a0e94228e34f00016ba03d0f09c6c95
Kitsune <KyuubiRan@qq.com>
2024-07-09 16:26:46 +0800

    Add RandomExtensions.cs

---
+++ FoxTail.Extensions/src/RandomExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace FoxTail.Extensions;
+
+public static class RandomExtensions
+{
+    public static float NextSingle(this Random random, float max)
+    {
+        return random.NextSingle() * max;
+    }
+
+    public static float NextSingle(this Random random, float min, float max)
+    {
+        if (min > max) throw new ArgumentException("Min value must be less than max value.");
+        return random.NextSingle() * (max - min) + min;
+    }
+
+    public static double NextDouble(this Random random, double max)
+    {
+        return random.NextDouble() * max;
+    }
+
+    public static double NextDouble(this Random random, double min, double max)
+    {
+        if (min > max) throw new ArgumentException("Min value must be less than max value.");
+        return random.NextDouble() * (max - min) + min;
+    }
+
+    public static int Next(this Random random, Range range)
+    {
+        return random.Next(range.Start.Value, range.End.Value);
+    }
+}
---
+++ FoxTail.UnitTests/src/Extensions/RandomExtensionsTest.cs
@@ -0,0 +1,39 @@
+﻿using FoxTail.Extensions;
+
+namespace FoxTail.UnitTests.Extensions;
+
+public class RandomExtensionsTest
+{
+    [Test]
+    public void NextTest()
+    {
+        var random = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            var result = random.NextSingle(10);
+            Console.WriteLine(result);
+            Assert.IsTrue(result is >= 0 and < 10);
+        }
+        
+        for (int i = 0; i < 10; i++)
+        {
+            var result = random.NextSingle(-10, 10);
+            Console.WriteLine(result);
+            Assert.IsTrue(result is >= -10 and < 10);
+        }   
+        
+        for (int i = 0; i < 10; i++)
+        {
+            var result = random.NextDouble(1000);
+            Console.WriteLine(result);
+            Assert.IsTrue(result is >= 0 and < 1000);
+        }
+        
+        for (int i = 0; i < 10; i++)
+        {
+            var result = random.NextDouble(-100, 100);
+            Console.WriteLine(result);
+            Assert.IsTrue(result is >= -100 and < 100);
+        }
+    }
+}
