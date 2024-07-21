8beae03a4f3997f75280f71e2d78b25d5c2188f7
Kitsune <KyuubiRan@qq.com>
2024-07-21 14:51:27 +0800

    Add AsyncQueue

---
+++ FoxTail.Common/src/Threading/AsyncQueue.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Concurrent;
+
+namespace FoxTail.Common.Threading;
+
+public class AsyncQueue<T> : IDisposable
+{
+    private readonly SemaphoreSlim _sem = new(0);
+    private readonly ConcurrentQueue<T> _que = new();
+
+    public void Enqueue(T item)
+    {
+        _que.Enqueue(item);
+        _sem.Release();
+    }
+
+    public async Task<T> DequeueAsync(CancellationToken token = default)
+    {
+        while (true)
+        {
+            await _sem.WaitAsync(token);
+            if (_que.TryDequeue(out var item))
+            {
+                return item;
+            }
+        }
+    }
+
+    public int Count => _que.Count;
+    public bool IsEmpty => _que.IsEmpty;
+    public bool Contains(T item) => _que.Contains(item);
+
+    public void Clear()
+    {
+        while (_que.TryDequeue(out _))
+        {
+        }
+
+        _sem.Release(_sem.CurrentCount);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _sem.Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ FoxTail.UnitTests/src/Common/AsyncQueueTest.cs
@@ -0,0 +1,33 @@
+﻿using FoxTail.Common.Threading;
+
+namespace FoxTail.UnitTests.Common;
+
+public class AsyncQueueTest
+{
+    [Test]
+    public void Test1()
+    {
+        const int end = 5;
+        var que = new AsyncQueue<int>();
+
+        var t1 = Task.Run(async () =>
+        {
+            for (var i = 0; i < end; i++)
+            {
+                await Task.Delay(500); 
+                que.Enqueue(i);
+                Console.WriteLine($"-> Enqueue: {i}, Time: {DateTime.Now:HH:mm:ss.fff}");
+            }
+        });
+
+        var t2 = Task.Run(async () =>
+        {
+            for (var i = 0; i < end; i++)
+            {
+                Console.WriteLine($"<- Dequeue: {await que.DequeueAsync()}, Time: {DateTime.Now:HH:mm:ss.fff}");
+            }
+        });
+
+        Task.WaitAll(t1, t2);
+    }
+}
