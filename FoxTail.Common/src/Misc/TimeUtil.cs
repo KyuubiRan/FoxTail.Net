8ad4a197b1163dfcbbdc804fa458c0e2d7670813
Kitsune <KyuubiRan@qq.com>
2024-07-29 20:42:17 +0800

    Add DateTimeFromMillis

--- FoxTail.Common/src/Misc/TimeUtil.cs
+++ FoxTail.Common/src/Misc/TimeUtil.cs
@@ -2,6 +2,14 @@
 
 public static class TimeUtil
 {
-    public static ulong CurrentTimeMillisUtc => (ulong)DateTime.UtcNow.Subtract(DateTime.UnixEpoch).TotalMilliseconds;
-    public static ulong CurrentTimeMillis => (ulong)DateTime.Now.Subtract(DateTime.UnixEpoch).TotalMilliseconds;
+    public static long CurrentTimeMillisUtc => (long)DateTime.UtcNow.Subtract(DateTime.UnixEpoch).TotalMilliseconds;
+    public static long CurrentTimeMillis => (long)DateTime.Now.Subtract(DateTime.UnixEpoch).TotalMilliseconds;   
+    
+    public static long CurrentTimeSecondsUtc => (long)DateTime.UtcNow.Subtract(DateTime.UnixEpoch).TotalSeconds;
+    public static long CurrentTimeSeconds => (long)DateTime.Now.Subtract(DateTime.UnixEpoch).TotalSeconds;
+    
+    public static DateTime DateTimeFromMillisUtc(long millis) => DateTime.UnixEpoch.AddMilliseconds(millis);
+    public static DateTime DateTimeFromMillis(long millis) => DateTime.UnixEpoch.AddMilliseconds(millis).ToUniversalTime();
+    public static DateTime DateTimeFromSecondsUtc(long seconds) => DateTime.UnixEpoch.AddSeconds(seconds);
+    public static DateTime DateTimeFromSeconds(long seconds) => DateTime.UnixEpoch.AddSeconds(seconds).ToUniversalTime();
 }
---
+++ FoxTail.UnitTests/src/Common/TimeUtilTest.cs
@@ -0,0 +1,24 @@
+﻿using FoxTail.Common.Misc;
+
+namespace FoxTail.UnitTests.Common;
+
+public class TimeUtilTest
+{
+    [Test]
+    public void CurrentTimeMillisTest()
+    {
+        Console.WriteLine($"CurrentTimeMillisUtc: {TimeUtil.CurrentTimeMillisUtc}");
+        Console.WriteLine($"CurrentTimeMillis: {TimeUtil.CurrentTimeMillis}");
+        Console.WriteLine($"CurrentTimeSecondsUtc: {TimeUtil.CurrentTimeSecondsUtc}");
+        Console.WriteLine($"CurrentTimeSeconds: {TimeUtil.CurrentTimeSeconds}");
+    }
+
+    [Test]
+    public void DateTimeTest()
+    {
+        Console.WriteLine($"DateTimeFromMillisUtc: {TimeUtil.DateTimeFromMillisUtc(TimeUtil.CurrentTimeMillisUtc)}");
+        Console.WriteLine($"DateTimeFromMillis: {TimeUtil.DateTimeFromMillis(TimeUtil.CurrentTimeMillis)}");
+        Console.WriteLine($"DateTimeFromSecondsUtc: {TimeUtil.DateTimeFromSecondsUtc(TimeUtil.CurrentTimeSecondsUtc)}");
+        Console.WriteLine($"DateTimeFromSeconds: {TimeUtil.DateTimeFromSeconds(TimeUtil.CurrentTimeSeconds)}");
+    }
+}
