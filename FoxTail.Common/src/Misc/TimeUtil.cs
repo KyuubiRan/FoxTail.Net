d107dcf1f0b3a93c13ba8568147c05be9f047eef
Kitsune <KyuubiRan@qq.com>
2024-07-29 15:21:41 +0800

    Add TimeUtil

---
+++ FoxTail.Common/src/Misc/TimeUtil.cs
@@ -0,0 +1,7 @@
+﻿namespace FoxTail.Common.Misc;
+
+public static class TimeUtil
+{
+    public static ulong CurrentTimeMillisUtc => (ulong)DateTime.UtcNow.Subtract(DateTime.UnixEpoch).TotalMilliseconds;
+    public static ulong CurrentTimeMillis => (ulong)DateTime.Now.Subtract(DateTime.UnixEpoch).TotalMilliseconds;
+}
