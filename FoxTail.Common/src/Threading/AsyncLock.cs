d9d05e46d105764b81eefd6a244187be4cfc6650
Kitsune <KyuubiRan@qq.com>
2024-08-10 09:32:16 +0800

    Add async lock & Bump version to 0.0.9

---
+++ FoxTail.Common/src/Threading/AsyncLock.cs
@@ -0,0 +1,44 @@
+﻿namespace FoxTail.Common.Threading;
+
+public sealed class AsyncLock : IDisposable
+{
+    private readonly SemaphoreSlim _sem = new(1, 1);
+
+    public struct LockReleaser(SemaphoreSlim s) : IDisposable
+    {
+        public void Release() => Dispose();
+
+        public void Dispose()
+        {
+            var sem = Interlocked.Exchange(ref s!, null);
+            sem.Release();
+        }
+    }
+
+    public async Task<LockReleaser> LockAsync(CancellationToken ct = default)
+    {
+        try
+        {
+            await _sem.WaitAsync(ct);
+            return new LockReleaser(_sem);
+        }
+        catch
+        {
+            _sem.Release();
+            throw;
+        }
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _sem.Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+}
---
+++ FoxTail.UnitTests/src/Common/AsyncLockTest.cs
@@ -0,0 +1,44 @@
+﻿using FoxTail.Common.Threading;
+
+namespace FoxTail.UnitTests.Common;
+
+public class AsyncLockTest
+{
+    [Test]
+    public async Task Test()
+    {
+        var asyncLock = new AsyncLock();
+
+        using (await asyncLock.LockAsync())
+        {
+            Console.WriteLine($"Locked at: {DateTime.Now}");
+            await Task.Delay(1000);
+        }
+
+        Console.WriteLine($"Unlocked at: {DateTime.Now}");
+
+        var t1 = Task.Run(async () =>
+        {
+            using (await asyncLock.LockAsync())
+            {
+                Console.WriteLine($"Locked at: {DateTime.Now}");
+                await Task.Delay(1000);
+            }
+
+            Console.WriteLine($"Unlocked at: {DateTime.Now}");
+        });
+
+        var t2 = Task.Run(async () =>
+        {
+            using (await asyncLock.LockAsync())
+            {
+                Console.WriteLine($"Locked at: {DateTime.Now}");
+                await Task.Delay(1000);
+            }
+
+            Console.WriteLine($"Unlocked at: {DateTime.Now}");
+        });
+        
+        await Task.WhenAll(t1, t2);
+    }
+}
